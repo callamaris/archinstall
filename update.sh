0117737e21cc4258ecb1ab470975ff036dbc2372
dczekala@googlemail.com <dczekala@googlemail.com>
2019-06-02 11:25:44 +0200

    [Finstall script

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 while IFS=, read -r tag programm comment; do
   echo $tag $programm $comment
+  case "$tag"
+    "") pacman --noconfirm --needed -S "$programm"
+    "A") sudo -u $(whoami) -S --noconfirm "$programm"
+  esac
 done < $HOME/.apps
