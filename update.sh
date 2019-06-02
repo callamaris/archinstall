670c343f5ea1474bdca0d14150f35e5cc73f5645
dczekala@googlemail.com <dczekala@googlemail.com>
2019-06-02 11:31:36 +0200

    fix

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@ while IFS=, read -r tag programm comment; do
   echo $tag $programm $comment
   case "$tag" in
     "") pacman --noconfirm --needed -S "$programm"
-    "A") sudo -u $(whoami) -S --noconfirm "$programm"
+    "A") sudo -u $USER -S --noconfirm "$programm"
   esac
 done < $HOME/.apps
